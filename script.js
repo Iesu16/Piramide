d95ef2ed2b529a9680542c019153b772990ae0a7
Iesu16 <136399963+Iesu16@users.noreply.github.com>
2023-11-08 18:23:18 -0500

    Update script.js
    optimizado

--- script.js
+++ script.js
@@ -6,17 +6,14 @@
             path: document.getElementById("path"),
         },
         init: () => {
-            const calcularButton = document.getElementById("Calcular"); 
-            calcularButton.addEventListener("click", GeneratePyramid.updatePyramid); 
-
-            GeneratePyramid.updatePyramid();
+            const calcularButton = document.getElementById("Calcular");
+            calcularButton.addEventListener("click", () => {GeneratePyramid.updatePyramid();});
         },
         updatePyramid: () => {
             const levels = GeneratePyramid.htmlElements.levels.value;
             const pyramid = GeneratePyramid.htmlElements.pyramid;
             pyramid.innerHTML = "";
             let input1 = "";
-
             let p = 0;
             for (let i = 1; i <= levels; i++) {
                 const row = document.createElement("div");
@@ -31,26 +28,17 @@
                 }
                 pyramid.appendChild(row);
             }
-            console.log(input1);
             let matrix = GeneratePyramid.getMatrixFromTriangle(input1);
-            let solvedMatrix = GeneratePyramid.getSolvedMatrix(GeneratePyramid.copyMatrix(matrix), 0);
-         /* console.log("matrix2");
-            console.table(matrix);
-            console.log("solved");
-            console.table(solvedMatrix);*/
+            let solvedMatrix = GeneratePyramid.getSolvedMatrix(GeneratePyramid.copyMatrix(matrix));
+            console.table(solvedMatrix);
             let maxLengthArr = GeneratePyramid.getLongestPathArr(solvedMatrix, matrix);
-
-            //console.log("arr", maxLengthArr);
             GeneratePyramid.htmlElements.path.innerHTML = GeneratePyramid.drawPath(maxLengthArr);
             GeneratePyramid.highlightLongestPath(matrix, maxLengthArr);
         },
         getMatrixFromTriangle(input) {
-  
             let array = input.split(/[ ]+/);
             let matrix = [];
-
             let j = 1;
-      
             let accIndex = 0;
             while (true) {
                 accIndex += j;
@@ -58,96 +46,54 @@
                 matrix.push([]);
                 let i = 0;
                 while (i < j) {
-                    try {
-                        matrix[j - 1].push(parseInt(array[(accIndex - j) + i]))
-                    }
-                    catch (err) {
-                        console.log(err)
-                        throw new Error("Valor en el arreglo no es numero")
-                    }
-                    i += 1;
+                 matrix[j - 1].push(parseInt(array[(accIndex - j) + i]))
+                 i += 1;
                 }
                 j += 1;
             }
-
-
             let max = matrix[matrix.length - 1].length;
-
             i = 0;
-
             while (i < matrix.length) {
-   
                 j = matrix[i].length;
                 while (j < max) {
-
                     matrix[i].push(0);
-
                     j += 1;
                 }
                 i += 1;
             }
             return matrix;
         },
-        getSolvedMatrix: (inputMatrix, order) => {
-            if (order == null || typeof (order) != "number") {
-                order = 0;
-                throw new Error("Order debe ser un numero.")
-            }
+        getSolvedMatrix: (inputMatrix) => {
             let newMatrix = inputMatrix;
-
-
             let row = newMatrix.length - 1, col = 0, valueOfComparison;
-
             while (row > 0) {
                 col = 0;
-
                 while (col < row) {
-     
-                    if (order == 0) {
                         valueOfComparison = GeneratePyramid.getMaxVal(newMatrix[row][col], newMatrix[row][col + 1])
-
-                    } else {
-                        valueOfComparison = GeneratePyramid.getMinVal(newMatrix[row][col], newMatrix[row][col + 1])
-                    }
                     newMatrix[row - 1][col] += valueOfComparison;
                     col += 1;
                 }
                 row--;
             }
-
             return newMatrix;
         },
         getLongestPathArr(solvedMatrix, matrix) {
             let pathArray = [];
             let row = 0, col = 0, searchValue;
-
             while (row < solvedMatrix.length) {
-
                 pathArray.push(matrix[row][col])
                 searchValue = solvedMatrix[row][col] - matrix[row][col];
-
                 if (row != solvedMatrix.length - 1) {
-
                     if (solvedMatrix[row + 1][col] != searchValue) {
                         col += 1
                     }
-
                 }
                 row++;
             }
-            // console.log(pathArray);
             return pathArray;
         },
         getMaxVal(a, b) {
             if (a > b) {
-                return a;
-            }
-            else {
-                return b;
-            }
-        },
-        getMinVal(a, b) {
-            if (a < b) {
                 return a;
             }
             else {
@@ -175,29 +121,22 @@
             drawnPath += `<span class="path-item squarer">${pathArr[pathArr.length - 1]}</span>
                             <span class="path-item ">=</span> 
                             <span class="path-item squarer">${sum + pathArr[pathArr.length - 1]}</span>`;
-        
             return drawnPath;
         },
-       
         highlightLongestPath(matrix, path) {
-            const pyramidRows = document.querySelectorAll('.row');
-        
+            const pyramidRows = document.querySelectorAll('.row');      
             for (let j = 0; j < path.length; j++) {
                 const row = pyramidRows[j];
                 const squares = row.querySelectorAll('.square');
-        
                 for (let i = 0; i < path.length; i++) {
                     const square = squares[i];
-        
                     if (matrix[j][i] === path[j]) {
                         square.style.backgroundColor = 'rgb(201, 131, 39)';
                     }
                 }
             }
         },
-        
     };
-
     GeneratePyramid.init();
     refreshButton.addEventListener("click", function() {
         const currentLevels = parseInt(GeneratePyramid.htmlElements.levels.value, 10);
